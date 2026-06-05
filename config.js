5547a1f97af77e83d96b5e78b3914484b284adfc
faratro96 <matteo.faratro1996@gmail.com>
2026-06-05 16:18:10 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // NON inserire mai la service_role key.
 
 window.CONVOCA_CONFIG = {
-  SUPABASE_URL: "INCOLLA_QUI_PROJECT_URL",
-  SUPABASE_ANON_KEY: "INCOLLA_QUI_ANON_PUBLIC_KEY",
+  SUPABASE_URL: "https://gmudehsnutkcjnchwlvp.supabase.co/rest/v1/",
+  SUPABASE_ANON_KEY: "sb_publishable_BBdYA1puofEC4NCBZeOCdg_KvVnitEi",
   APP_ID: "convoca-app-principale"
 };
